a02ee8913e38d255b5e3c807ba911a6aee8831ee
gyu-bin <rbqls6651@naver.com>
2025-06-24 12:56:04 +0900

    api key 수정

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,14 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'; // ✅ 스토리지도 초기화 가능
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAtkt7DLTq_LdYEsqiHEWgzZLGkBa4d2JI",
-    authDomain: "churchappexpo.firebaseapp.com",
-    projectId: "churchappexpo",
-    // storageBucket: "churchappexpo.appspot.com",
-    storageBucket: "gs://churchappexpo.firebasestorage.app",
-    messagingSenderId: "650084657232",
-    appId: "1:650084657232:web:a772970ae7b4154ff14955",
-    measurementId: "G-Z6F6G7BG5T"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
