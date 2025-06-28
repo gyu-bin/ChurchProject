b79410eaaed3de7cb42407aa85ba11bfa22388af
hypnotes <hayeongpark@korea.ac.kr>
2025-06-28 19:10:18 +0900

    feat: user type 추가

---
+++ constants/_types/user.ts
@@ -0,0 +1,21 @@
+type Campus = "문래" | "신촌" | "시선";
+type Division = "청년1부" | "청년2부" | "장년부";
+
+type Role = "관리자" | "정회원" | "새가족";
+
+// API return 값 타입
+export type DangerousUser = {
+  campus: Campus;
+  createdAt: {
+    nanoseconds: number;
+    seconds: number;
+  };
+  division: Division;
+  email: string;
+  expoPushTokens: string[];
+  name: string;
+  password: string; // 이거 여기 있어도 되는가?
+  role: Role;
+};
+
+export type User = Omit<DangerousUser, "password">;
