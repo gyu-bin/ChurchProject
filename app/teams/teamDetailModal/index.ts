2d82aee933274f23b4a47e6bf7e20fcc60866c2d
gyu-bin <rbqls6651@naver.com>
2025-06-28 21:23:42 +0900

    우선파일삭제

--- app/teams/teamDetailModal/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as EditTeamModal } from './EditTeamModal';
-export { default as LocationModal } from './LocationModal';
-export { default as VoteModal } from './VoteModal';
-export { default as DatePickerModal } from './DatePickerModal';
